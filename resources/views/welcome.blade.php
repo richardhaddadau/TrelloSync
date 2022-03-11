c4774521d21b1afeb3ff22338478cc15924f1cea
Richard Haddad <richard@haddads.net.au>
2022-03-12 05:02:05 +1000

    TailwindCss testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
 <body>
 
-    <h2>Trello Sync</h2>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 
 </body>
 </html>
